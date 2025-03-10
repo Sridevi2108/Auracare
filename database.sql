c955cf699df6d8023ec976492287483ef76194fb
Sridevi2108 <sridevi21082003@gmail.com>
2025-03-10 11:52:10 +0530

    Add files via upload

--- database.sql
+++ database.sql
@@ -2,8 +2,8 @@ CREATE DATABASE auracare_db;
 USE auracare_db;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 select * from users;
