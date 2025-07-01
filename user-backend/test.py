38d1883206d329c42a241d036935a23f6b0dc822
Sridevi <sridevi21082003@gmail.com>
2025-07-01 20:01:16 +0530

    Update test.py

--- user-backend/test.py
+++ user-backend/test.py
@@ -1,7 +1,7 @@
 import requests
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-HEADERS = {"Authorization": "Bearer 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"}
+API_URL = "YOUR API URL"
+HEADERS = {"Authorization": "Bearer hf_xxxxxxxxxx"}
 PAYLOAD = {"inputs": "Hello, how are you?"}
 
 response = requests.post(API_URL, json=PAYLOAD, headers=HEADERS)
